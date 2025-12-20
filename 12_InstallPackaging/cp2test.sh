7114c979cd65eddc6c9f5c29ac4b65986063e76b
Dugit0 <dmitry.astrakhantsev@gmail.com>
2025-12-20 14:28:16 +0300

    Add installation

---
+++ 12_InstallPackaging/cp2test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+# rm -r /home/dmitry/SemM1/linuxdev25/12_InstallPackaging/build/*
+sudo cp -r /home/dmitry/SemM1/linuxdev25/12_InstallPackaging /home/testuser
+sudo chown -R testuser:testuser /home/testuser/12_InstallPackaging
